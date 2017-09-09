335c7825ad0eaa8cb9da4430d18d615e18fc004e
mambax7 <mambax7@gmail.com>
2017-09-09 11:07:35 -0400

    Menu standardization

--- admin/menu.php
+++ admin/menu.php
@@ -14,39 +14,49 @@ if (false !== ($moduleHelper = Xmf\Module\Helper::getHelper($moduleDirName))) {
     $moduleHelper = Xmf\Module\Helper::getHelper('system');
 }
 
-
 $pathIcon32 = \Xmf\Module\Admin::menuIconPath('');
 //$pathModIcon32 = $moduleHelper->getModule()->getInfo('modicons32');
 
 $moduleHelper->loadLanguage('modinfo');
 
-$adminmenu              = [];
-$i                      = 0;
-'title' =>  _AM_MODULEADMIN_HOME,
-'link' =>  'admin/index.php',
-'icon' =>  $pathIcon32 . '/home.png',
-++$i;
-'title' =>  _MI_SPARTNER_ADMENU1,
-'link' =>  'admin/main.php',
-'icon' =>  $pathIcon32 . '/manage.png',
+$adminmenu[] = [
+    'title' => _AM_MODULEADMIN_HOME,
+    'link'  => 'admin/index.php',
+    'icon'  => $pathIcon32 . '/home.png',
+];
 
-++$i;
-'title' =>  _MI_SPARTNER_CATEGORIES,
-'link' =>  'admin/category.php',
-'icon' =>  $pathIcon32 . '/category.png',
-++$i;
-'title' =>  _MI_SPARTNER_ADMENU2,
-'link' =>  'admin/partner.php',
-'icon' =>  $pathIcon32 . '/users.png',
-++$i;
-'title' =>  _MI_SPARTNER_ADMENU3,
-'link' =>  'admin/offer.php',
-'icon' =>  $pathIcon32 . '/cash_stack.png',
-++$i;
-'title' =>  _MI_SPARTNER_IMPORT,
-'link' =>  'admin/import.php',
-'icon' =>  $pathIcon32 . '/compfile.png',
-++$i;
-'title' =>  _AM_MODULEADMIN_ABOUT,
-'link' =>  'admin/about.php',
-'icon' =>  $pathIcon32 . '/about.png',
+$adminmenu[] = [
+    'title' => _MI_SPARTNER_ADMENU1,
+    'link'  => 'admin/main.php',
+    'icon'  => $pathIcon32 . '/manage.png',
+];
+
+$adminmenu[] = [
+    'title' => _MI_SPARTNER_CATEGORIES,
+    'link'  => 'admin/category.php',
+    'icon'  => $pathIcon32 . '/category.png',
+];
+
+$adminmenu[] = [
+    'title' => _MI_SPARTNER_ADMENU2,
+    'link'  => 'admin/partner.php',
+    'icon'  => $pathIcon32 . '/users.png',
+];
+
+$adminmenu[] = [
+    'title' => _MI_SPARTNER_ADMENU3,
+    'link'  => 'admin/offer.php',
+    'icon'  => $pathIcon32 . '/cash_stack.png',
+];
+
+$adminmenu[] = [
+    'title' => _MI_SPARTNER_IMPORT,
+    'link'  => 'admin/import.php',
+    'icon'  => $pathIcon32 . '/compfile.png',
+];
+
+$adminmenu[] = [
+    'title' => _AM_MODULEADMIN_ABOUT,
+    'link'  => 'admin/about.php',
+    'icon'  => $pathIcon32 . '/about.png',
+];
